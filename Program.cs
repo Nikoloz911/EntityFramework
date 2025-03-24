b926bde39cd6d185c8cd95fc05ab767b4cc2a3ed
Nikoloz911 <nikalobjanidze014@gmail.com>
2025-03-25 00:14:20 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.CodeAnalysis;
+
+Console.WriteLine("Microsoft EntityFramework Nuget Packages");
+
+Console.WriteLine("Microsoft.EntityFrameworkCore (8.0.0)");
+Console.WriteLine("Microsoft.EntityFrameworkDesign (8.0.0)");
+Console.WriteLine("Microsoft.EntityFrameworkSqlServer (8.0.0)");
+Console.WriteLine("Microsoft.EntityFrameworkTools (8.0.0)");
+
+
+
+//< Project Sdk = "Microsoft.NET.Sdk" >
+
+//  < PropertyGroup >
+//    < OutputType > Exe </ OutputType >
+//    < TargetFramework > net8.0 </ TargetFramework >
+//    < ImplicitUsings > enable </ ImplicitUsings >
+//    < Nullable > enable </ Nullable >
+//  </ PropertyGroup >
+
+//    < ItemGroup >
+
+//        < PackageReference Include = "Microsoft.EntityFrameworkCore.Design" Version = "8.0.0" />
+
+//        < PackageReference Include = "Microsoft.EntityFrameworkCore" Version = "8.0.0" />
+
+//        < PackageReference Include = "Microsoft.EntityFrameworkCore.SqlServer" Version = "8.0.0" />
+
+//        < PackageReference Include = "Microsoft.EntityFrameworkCore.Tools" Version = "8.0.0" >
+
+//            < IncludeAssets > runtime; build; native; contentfiles; analyzers; buildtransitive </ IncludeAssets >
+
+//            < PrivateAssets ></ PrivateAssets >
+
+//            < Pack > true </ Pack >
+
+//        </ PackageReference >
+
+//    </ ItemGroup >
+//</ Project >
